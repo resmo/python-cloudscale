3f46dc10126d6593fa28d4d75d88374aab7b9c04
Rene Moser <mail@renemoser.net>
2019-11-21 23:36:01 +0100

    extend network support

--- cloudscale/commands/network.py
+++ cloudscale/commands/network.py
@@ -1,6 +1,6 @@
 import sys
 import click
-from ..util import to_table
+from ..util import to_table, to_pretty_json, to_dict
 from .. import Cloudscale, CloudscaleApiException, CloudscaleException
 
 @click.group()
@@ -24,6 +24,69 @@ def cmd_list(cloudscale):
             headers = ['name', 'created_at', 'zone', 'tags', 'uuid']
             table = to_table(response, headers)
             click.echo(table)
+    except CloudscaleApiException as e:
+        click.echo(e, err=True)
+        sys.exit(1)
+
+@click.option('--uuid', required=True)
+@network.command("show")
+@click.pass_obj
+def cmd_show(cloudscale, uuid):
+    try:
+        response = cloudscale.network.get_by_uuid(uuid)
+        click.echo(to_pretty_json(response))
+    except CloudscaleApiException as e:
+        click.echo(e, err=True)
+        sys.exit(1)
+
+@click.option('--name', required=True)
+@click.option('--zone')
+@click.option('--mtu', type=int, default=9000)
+@click.option('--auto-create-ipv4-subnet', type=bool, default=True)
+@click.option('--tags', multiple=True)
+@network.command("create")
+@click.pass_obj
+def cmd_create(cloudscale, name, zone, mtu, auto_create_ipv4_subnet, tags):
+    try:
+        response = cloudscale.network.create(
+            name=name,
+            zone=zone,
+            mtu=mtu,
+            auto_create_ipv4_subnet=auto_create_ipv4_subnet,
+            tags=to_dict(tags),
+        )
+        click.echo(to_pretty_json(response))
+    except CloudscaleApiException as e:
+        click.echo(e, err=True)
+        sys.exit(1)
+
+@click.option('--uuid', required=True)
+@click.option('--name')
+@click.option('--mtu')
+@click.option('--tags', multiple=True)
+@network.command("update")
+@click.pass_obj
+def cmd_update(cloudscale, uuid, name, mtu, tags):
+    try:
+        cloudscale.network.update(
+            uuid=uuid,
+            name=name,
+            mtu=mtu,
+            tags=to_dict(tags),
+        )
+        response = cloudscale.network.get_by_uuid(uuid)
+        click.echo(to_pretty_json(response))
+    except CloudscaleApiException as e:
+        click.echo(e, err=True)
+        sys.exit(1)
+
+@click.option('--uuid', required=True)
+@network.command("delete")
+@click.pass_obj
+def cmd_delete(cloudscale, uuid):
+    try:
+        cloudscale.network.delete(uuid)
+        click.echo("Deleted!")
     except CloudscaleApiException as e:
         click.echo(e, err=True)
         sys.exit(1)
--- cloudscale/lib/network.py
+++ cloudscale/lib/network.py
@@ -5,3 +5,21 @@ class Network(CloudscaleMutable):
     def __init__(self):
         super().__init__()
         self.resource = 'networks'
+
+    def create(self, name, zone, mtu=None, auto_create_ipv4_subnet=None, tags=None):
+        payload = {
+            'name': name,
+            'zone': zone,
+            'mtu': mtu,
+            'auto_create_ipv4_subnet': auto_create_ipv4_subnet,
+            'tags': tags,
+        }
+        return super().create(payload=payload)
+
+    def update(self, uuid, name=None, mtu=None, tags=None):
+        payload = {
+            'name': name,
+            'mtu': mtu,
+            'tags': tags,
+        }
+        return super().update(uuid=uuid, payload=payload)
